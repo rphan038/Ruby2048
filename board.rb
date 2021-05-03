9d70cf3a302cfdee41dce1d25ea7a14c1706cfa6
richard <phanr@lafayette.edu>
2021-05-03 19:15:07 -0400

    Added comments

--- board.rb
+++ board.rb
@@ -1,4 +1,5 @@
 class Board
+    # Tile inner class. The Board class is composed of many instances of Tile
     class Tile
         @@tileTypes = [0, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
         @value
@@ -45,8 +46,10 @@ class Board
         end
     end
 
+    # Board Instance Variable
     @board
 
+    # Board Constructor
     def initialize
         @board = Array.new(4) { [Tile.new(0), Tile.new(0), Tile.new(0), Tile.new(0)] }
 
@@ -57,14 +60,17 @@ class Board
         #     Tile.new(2), Tile.new(8), Tile.new(16), Tile.new(4)]]
     end
 
+    # Getter method for the BOARD instance variable
     def getBoard
         @board
     end
 
+    # Helper method for the move methods
     def flipBoard
         @board.transpose.map(&:reverse)
     end
 
+    # Move all tiles left one index if possible. If the tile is on the leftmost column, stay there
     def shift_left(line)
         new_line = []
         line.each { |line| new_line << line unless line.getTileVal.zero? }
@@ -112,10 +118,12 @@ class Board
         action
     end
 
+    # Checks to see whether the player has won
     def winChecker
         @board.flatten.max.getTileVal == 2048
     end
 
+    # Checks the board to see whether there are no more legal moves
     def lossChecker
         new_board = Marshal.load(Marshal.dump(@board))
         option = moveRight || moveLeft || moveUp || moveDown
@@ -128,12 +136,14 @@ class Board
         false
     end
 
+    # Changes the value of every tile on the board to 0
     def resetBoard
         (0..3).each do |x|
             (0..3).each { |y| @board[x][y].setTileVal(0) }
         end
     end
 
+    # Converts the board to a string
     def stringify
         str = ""
         (0..3).each do |x|
@@ -144,6 +154,7 @@ class Board
         return str
     end
 
+    # Generates a new tile where there is a tile with value 0
     def newTile
         tile = [*1..2].sample * 2
         x = [*0..3].sample
@@ -171,6 +182,7 @@ class Board
     end
 end
 
+#Testing
 t = Board::Tile.new(2)
 t1 = Board::Tile.new(4)
 puts t.getTileVal
